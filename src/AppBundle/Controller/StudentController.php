cfe567fe9165390519b425236af6de0e61ce0110
liado <liado@wp.pl>
2018-06-12 14:30:47 +0200

    Added student/update/{id}

--- src/AppBundle/Controller/StudentController.php
+++ src/AppBundle/Controller/StudentController.php
@@ -12,14 +12,26 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType; 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
+use Psr\Log\LoggerInterface;
+
 class StudentController extends Controller { 
+    /* Doesn't work.
+    public $students;
+    public $classes;
+
+    function __construct(){
+        $doct = $this->getDoctrine()->getManager(); //FatalThrowableErrorHTTP 500 Internal Server Error Call to a member function has() on null
+        $this->$students = $doct->getRepository('AppBundle:Student');
+        $this->$classes = $doct->getRepository('AppBundle:Classes');
+    }
+     */
    /** 
    * @Route("/student/display") 
 */ 
 public function displayAction(Request $request) { 
     $stud = new Student();
     $connection = $this->getDoctrine()->getManager()->getConnection();
-    $classesList= $connection->prepare("SELECT * FROM classes");
+    $classesList = $connection->prepare("SELECT * FROM classes");
     $classesList->execute();
     $studentId = array();
     $classId = array();
@@ -35,9 +47,6 @@ public function displayAction(Request $request) {
             $classId[] = $data;
         }
     }
-
-
-
     // Replace duplicate names in the students name column with '--'
     $oldName = "";
     for ($i = 0 ; $i < count($studentId); $i++ ){
@@ -76,7 +85,7 @@ public function displayAction(Request $request) {
     return $this->render('student/display.html.twig', array('data' => $stud, 'form' => $form->createView(), 'students' => $studentId, 'classes' => $classId, )); 
  } 
  /** 
-   * @Route("/student/delete/{id}", name="student_delete") 
+   * @Route("/student/delete/{id}", name="student_delete", requirements={"id"="\d+"}) 
 */ 
 public function deleteAction($id) { 
     $doct = $this->getDoctrine()->getManager(); 
@@ -88,5 +97,75 @@ public function deleteAction($id) {
     $doct->remove($stud); 
     $doct->flush(); 
     return $this->redirect("/student/display");
- } 
+ }
+  /** 
+   * @Route("/student/update/{id}", name="student_update", requirements={"id"="\d+"}) 
+*/ 
+public function updateAction($id, LoggerInterface $logger) { 
+    $doct = $this->getDoctrine()->getManager(); 
+    $stud = $doct->getRepository('AppBundle:Student')->find($id);
+    if (!$stud) {  
+        $this->addFlash(
+            'notice',
+            'No student for id ' . $id . '.'
+        );
+        return $this->redirect("/student/display");
+     } 
+    $connection = $this->getDoctrine()->getManager()->getConnection();
+    $allClassesQuery = $connection->prepare("SELECT * FROM class");
+    $allClassesQuery->execute();
+    $allClasses = array();
+    while ($row = $allClassesQuery->fetch()){
+        $allClasses[] = $row;
+    }
+    $classes = $stud->getClasses();
+    
+    for($i = 0 ; $i < count($allClasses); $i++){
+        $allClasses[$i]["isAttending"] = FALSE; // First add the "isAttending" field to each class, so it exists.
+        for($j = 0 ; $j < count($classes); $j++){
+            //$logger->info($classes[$j]->getId() . ' ' . $allClasses[$i]['id']);
+            if($classes[$j]->getId() == $allClasses[$i]['id']){
+                $allClasses[$i]["isAttending"] = TRUE; 
+                break;    
+            } else {
+                $allClasses[$i]["isAttending"] = FALSE;
+            }
+        }
+    }    
+
+    return $this->render("student/update.html.twig", array('student' => $stud, 'allClasses' => $allClasses, 'classes' => $classes ));
+}
+ /** 
+   * @Route("/student/update/{id}/{classId}/{action}", requirements={"id"="\d+", "classId"="\d+"}) 
+*/ 
+public function updateClassAction($id, $classId, $action) { 
+    $doct = $this->getDoctrine()->getManager(); 
+    $stud = $doct->getRepository('AppBundle:Student')->find($id); 
+    if (!$stud) {  
+        $this->addFlash(
+            'notice',
+            'No student for id ' . $id . '.'
+        );
+        return $this->redirect("/student/display");
+     } 
+    $class = $doct->getRepository('AppBundle:Classes')->find($classId); 
+    if (!$class) { 
+        $this->addFlash(
+            'notice',
+            'No class for id ' . $classId . '.'
+        );
+        return $this->redirect("/student/display");
+     } 
+    if($action == "rm"){
+        $stud->removeClass($class);
+        $doct->flush();
+     } else if ($action == "add"){
+        $stud->addClass($class);
+        $doct->flush();
+     } else {
+        return $this->redirect("/student/update/$id");
+     }
+
+    return $this->redirect("/student/update/$id");
+    }  
 } 
