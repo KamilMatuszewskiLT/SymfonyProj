54971c01b0486e2fdbae9afaf54f4faa3b643b77
liado <liado@wp.pl>
2018-06-12 15:53:33 +0200

    End of day commit

--- src/AppBundle/Controller/StudentController.php
+++ src/AppBundle/Controller/StudentController.php
@@ -137,35 +137,5 @@ public function updateAction($id, LoggerInterface $logger) {
 }
  /** 
    * @Route("/student/update/{id}/{classId}/{action}", requirements={"id"="\d+", "classId"="\d+"}) 
-*/ 
-public function updateClassAction($id, $classId, $action) { 
-    $doct = $this->getDoctrine()->getManager(); 
-    $stud = $doct->getRepository('AppBundle:Student')->find($id); 
-    if (!$stud) {  
-        $this->addFlash(
-            'notice',
-            'No student for id ' . $id . '.'
-        );
-        return $this->redirect("/student/display");
-     } 
-    $class = $doct->getRepository('AppBundle:Classes')->find($classId); 
-    if (!$class) { 
-        $this->addFlash(
-            'notice',
-            'No class for id ' . $classId . '.'
-        );
-        return $this->redirect("/student/display");
-     } 
-    if($action == "rm"){
-        $stud->removeClass($class);
-        $doct->flush();
-     } else if ($action == "add"){
-        $stud->addClass($class);
-        $doct->flush();
-     } else {
-        return $this->redirect("/student/update/$id");
-     }
-
-    return $this->redirect("/student/update/$id");
-    }  
+*/  
 } 
